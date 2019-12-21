d705fbb442dae8ac6b6473848b14a83715cc3246
Maxime Moreillon <m.moreillon@gmail.com>
2019-12-21 17:20:46 +0000

    fixed conflict

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -43,50 +42,3 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
-=======
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-
-import VueQuillEditor, {Quill} from 'vue-quill-editor'
-import ImageResize from 'quill-image-resize-module';
-import { ImageDrop } from 'quill-image-drop-module'
-
-import '@mdi/font/css/materialdesignicons.css';
-
-// require styles
-import 'quill/dist/quill.core.css'
-import 'quill/dist/quill.snow.css'
-import 'quill/dist/quill.bubble.css'
-
-axios.defaults.withCredentials = true  // enable axios post cookie, default false
-axios.defaults.crossDomain = true
-
-// Use inline css instead of quill classes
-Quill.register(Quill.import('attributors/style/align'), true);
-Quill.register(Quill.import('attributors/class/color'), true);
-Quill.register(Quill.import('attributors/style/size'), true);
-
-Quill.register('modules/imageResize', ImageResize)
-Quill.register('modules/imageDrop', ImageDrop)
-
-Vue.use(VueQuillEditor, /* { default global options } */)
-Vue.use(VueAxios, axios)
-
-Vue.config.productionTip = false
-
-
-router.beforeEach((to, from, next) => {
-  next();
-});
-
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
->>>>>>> 90ea92786b0efb4dd03177726c7d37bc7de8c838
