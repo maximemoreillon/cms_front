0d91da727a51aaa33c8e654364fe559127c7dd40
Maxime Moreillon <m.moreillon@gmail.com>
2020-03-11 04:23:38 +0000

    previous commit was a bad idea

--- src/views/ArticleList.vue
+++ src/views/ArticleList.vue
@@ -347,7 +347,7 @@ export default {
 
   /* Normal behavior */
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(400px,1fr));
+  grid-template-columns: repeat(auto-fit, minmax(300px,1fr));
   grid-gap: 15px;
 }
 
