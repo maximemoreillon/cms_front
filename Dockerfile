ef540197338944ff56ea8c8e3d33f1b5146c4839
Maxime Moreillon <m.moreillon@gmail.com>
2020-07-11 13:00:30 +0000

    now using env variables from k8s

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
+# Build the Vue app
 FROM node:latest as build-stage
 WORKDIR /app
 COPY package*.json ./
@@ -6,7 +7,13 @@ RUN npm install
 COPY ./ .
 RUN npm run build
 
+# Put the built app in an NGINX contaier
 FROM nginx as production-stage
 RUN mkdir /app
 COPY --from=build-stage /app/dist /app
 COPY nginx.conf /etc/nginx/nginx.conf
+
+# Custom stuff
+COPY ./entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+ROOT_DIR=/app
+
+# Replace env vars in files served by NGINX
+echo "Replacing environment variables"
+for file in $ROOT_DIR/js/*.js* $ROOT_DIR/index.html $ROOT_DIR/precache-manifest*.js;
+do
+  echo "Processing $file ...";
+
+  sed -i 's|VUE_APP_CMS_API_URL|'${VUE_APP_CMS_API_URL}'|g' $file
+  sed -i 's|VUE_APP_AUTHENTICATION_API_URL|'${VUE_APP_AUTHENTICATION_API_URL}'|g' $file
+  sed -i 's|VUE_APP_AUTHENTICATION_FRONT_URL|'${VUE_APP_AUTHENTICATION_FRONT_URL}'|g' $file
+
+done
+
+echo "Starting Nginx"
+nginx -g 'daemon off;'
