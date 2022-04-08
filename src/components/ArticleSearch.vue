ae644f17273611e468c6a3cb5cf38e199f516357
Maxime Moreillon <m.moreillon@gmail.com>
2022-04-08 22:46:14 +0000

    improved search

--- src/components/ArticleSearch.vue
+++ src/components/ArticleSearch.vue
@@ -8,7 +8,6 @@
       class="search_bar"
       ref="search"
       placeholder="Search articles"
-      :class="{ open }"
       v-model="search_string">
 
     <button
@@ -27,32 +26,21 @@ export default {
   },
   data(){
     return {
-      open: false,
       search_string: '',
     }
   },
   methods: {
     search(){
-      if(this.open){
-        // if the search bar is open, search or close
 
-        const query = {...this.$route.query, search: this.search_string}
+      const query = {...this.$route.query, search: this.search_string}
 
-        if(this.search_string === '') {
-          this.open = false
-          delete query.search
-        }
+      if(this.search_string === '') delete query.search
 
-        if(this.$route.query.search === query.search) return
+      if(this.$route.query.search === query.search) return
 
-        this.$router.push({name: 'articles', query})
+      this.$router.push({name: 'articles', query})
 
-      }
-      else {
-        // If the search bar is closed, open it
-        this.open = true
-        setTimeout(() => this.$refs.search.focus(),50)
-      }
+
 
     }
   }
@@ -73,11 +61,6 @@ export default {
   padding: 0.5em;
 }
 
-.search_bar.open {
-  /* DIRTY */
-  width: 20vw;
-  visibility: visible;
-}
 
 
 </style>
