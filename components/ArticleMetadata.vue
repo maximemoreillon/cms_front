b7a8f4aa5a014da2c21c1a842ee0dec30ec6eb51
Maxime Moreillon <m.moreillon@gmail.com>
2022-06-25 00:13:04 +0000

    article deletion

--- components/ArticleMetadata.vue
+++ components/ArticleMetadata.vue
@@ -3,61 +3,50 @@
 
     <div class="metadata_wrapper">
       <!-- Author -->
-      <div
-        class="metadata_element"
-        title="Author">
-        <MaterialIconAccount class="metadata_icon"/>
-        <Author v-bind:author="article.author"/>
+      <div class="metadata_element" title="Author">
+        <MaterialIconAccount class="metadata_icon" />
+        <Author v-bind:author="article.author" />
       </div>
       <!-- Creation date -->
-      <div
-        class="metadata_element"
-        title="Created"
-        v-if="article.authorship.creation_date">
-        <MaterialIconCalendar class="metadata_icon"/>
+      <div class="metadata_element" title="Created" v-if="article.authorship.creation_date">
+        <MaterialIconCalendar class="metadata_icon" />
         <span>{{format_date(article.authorship.creation_date)}}</span>
       </div>
       <!-- Last edited date -->
-      <div
-        class="metadata_element"
-        title="last edited"
-        v-if="article.authorship.edition_date">
-        <MaterialIconCalendarEdit class="metadata_icon"/>
+      <div class="metadata_element" title="last edited" v-if="article.authorship.edition_date">
+        <MaterialIconCalendarEdit class="metadata_icon" />
         <span>{{format_date(article.authorship.edition_date)}}</span>
       </div>
 
       <!-- View count -->
-      <div
-        class="metadata_element"
-        title="Views"
-        v-if="article.views">
-        <MaterialIconEye class="metadata_icon"/>
+      <div class="metadata_element" title="Views" v-if="article.views">
+        <MaterialIconEye class="metadata_icon" />
         <span>{{article.views}}</span>
       </div>
 
       <!-- Published indicator -->
       <template v-if="user_is_author">
-        <div
-          class="metadata_element"
-          title="Published"
-          v-if="article.published">
-          <MaterialIconEarth class="metadata_icon"/>
+        <div class="metadata_element" title="Published" v-if="article.published">
+          <MaterialIconEarth class="metadata_icon" />
           <span>Published</span>
         </div>
 
-        <div
-          class="metadata_element"
-          title="Private"
-          v-if="!article.published">
-          <MaterialIconLock class="metadata_icon"/>
+        <div class="metadata_element" title="Private" v-if="!article.published">
+          <MaterialIconLock class="metadata_icon" />
           <span>Private</span>
         </div>
+        
+        <div class="spacer"></div>
+
+        <button @click="delete_article()">
+          <MaterialIconDelete class="metadata_icon" />
+          <span>Delete</span>
+        </button>
 
         <!-- Link to editor -->
-        <router-link
-          :to="{ name: 'articles-id-edit', params: { id: article._id } }"
+        <router-link :to="{ name: 'articles-id-edit', params: { id: article._id } }"
           class="metadata_element edit_button button">
-          <MaterialIconPencil class="metadata_icon"/>
+          <MaterialIconPencil class="metadata_icon" />
           <span>Edit</span>
         </router-link>
       </template>
@@ -65,18 +54,12 @@
 
 
 
-    <div
-      class="metadata_element tags_wrapper"
-      v-if="article.tags.length">
+    <div class="metadata_element tags_wrapper" v-if="article.tags.length">
 
-      <MaterialIconTag class="metadata_icon"/>
+      <MaterialIconTag class="metadata_icon" />
 
       <div class="tags_container">
-        <Tag
-          class="tag"
-          v-for="(tag, index) in article.tags"
-          v-bind:key="`tag_${index}`"
-          v-bind:tag="tag"/>
+        <Tag class="tag" v-for="(tag, index) in article.tags" v-bind:key="`tag_${index}`" v-bind:tag="tag" />
       </div>
 
 
@@ -110,7 +93,22 @@ export default {
         month.toString().padStart(2,'0'),
         day.toString().padStart(2,'0'),
       ].join('/');
-    }
+    },
+    async delete_article() {
+      if(!confirm(`Delete article?`)) return
+      const url = `${this.$config.apiUrl}/v1/articles/${this.article_id}`
+
+
+      try {
+        await this.$axios.delete(url)
+        this.$router.push({ name: 'index' })
+      } catch (error) {
+        if (error.response) alert(error.response.data)
+        else alert(error)
+      }
+
+
+    },
   },
   computed: {
     user_is_author(){
@@ -120,6 +118,9 @@ export default {
       const author_id = this.article.author._id
 
       return current_user_id === author_id
+    },
+    article_id(){
+      return this.$route.params.id
     }
 
   }
@@ -162,10 +163,6 @@ export default {
   gap: 0.25em;
 }
 
-
-.edit_button {
-  margin-left: auto;
-}
 
 .metadata_icon {
   margin-right: 0.25em;
--- modules/material-design-icons.js
+++ modules/material-design-icons.js
@@ -7,6 +7,7 @@ const icons = [
   'CalendarEdit',
   'Close',
   'ContentSave',
+  'Delete',
   'Eye',
   'FileDocumentOutline',
   'Earth',
