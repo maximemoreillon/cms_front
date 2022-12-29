15882438ad5326bb0e7fd4f3b6c1bc795ce00812
Maxime Moreillon <m.moreillon@gmail.com>
2022-12-29 22:47:31 +0900

    minor

--- layouts/default.vue
+++ layouts/default.vue
@@ -188,8 +188,9 @@ main a {
 article .content img {
   max-width: 80%;
   max-height: 60vh;
-  border-radius: var(--border-radius);
+  /* border-radius: var(--border-radius); */
   /* box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23); */
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
 }
 
 /* responsivity */
