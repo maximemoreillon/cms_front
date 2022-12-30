129451912d9dadf47c34dc4f552662ec53acff66
Maxime Moreillon <m.moreillon@gmail.com>
2022-12-30 12:49:00 +0900

    about

---
+++ pages/about.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>About</h1>
+  <p>CMS GUI v{{ versionRef }}</p>
+  <p>Developed by Maxime Moreillon</p>
+</template>
+
+<script lang="ts" setup>
+import { version } from "../package.json"
+
+const versionRef = ref(version)
+</script>
