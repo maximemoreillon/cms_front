12b276a2a3f502e4b7534f591ec1a7f0106caacc
Maxime Moreillon <m.moreillon@gmail.com>
2022-12-31 09:07:42 +0900

    fixed seo meta

--- appHead.ts
+++ appHead.ts
@@ -1,17 +1,27 @@
 const { CANONICAL_URL } = process.env
 
+const title = "Articles | Maxime Moreillon"
 const description = "An article management system developed by Maxime Moreillon"
 
 export default {
-  title: "Articles | Maxime Moreillon",
-  //   TODO: make favicon user customizable
-  link: [{ rel: "icon", type: "image/png", href: "/images/logo.png" }],
+  title,
+  link: [
+    { rel: "icon", type: "image/png", href: "/images/logo.png" }, // TODO: make favicon user customizable
+  ],
   meta: [
-    { name: "description", content: "Articles | Maxime Moreillon" },
+    { name: "description", content: description },
+
+    // OpenGraph
+    { name: "og:title", content: title },
+    { name: "og:description", content: description },
+    { name: "og:image", content: `${CANONICAL_URL}/logo.png` },
+    { name: "og:url", content: CANONICAL_URL },
+    { name: "og:type", content: "article" },
+    { name: "og:locale", content: "en_US" },
 
     // Twitter
     { name: "twitter:card", content: "summary" },
-    { name: "twitter:site", content: "@m_moreillon" },
+    { name: "twitter:site", content: "@m_moreillon" }, // TODO: make twitter username user customizable
     { name: "twitter:title", content: "Articles | Maxime Moreillon" },
     {
       name: "twitter:description",
@@ -22,20 +32,5 @@ export default {
       content: `${CANONICAL_URL}/logo.png`,
     },
     { name: "twitter:image:alt", content: "CMS logo" },
-
-    // OpenGraph
-    {
-      name: "og:title",
-      content: description,
-    },
-    { name: "og:description", content: "Articles | Maxime Moreillon" },
-    //TODO: use logo in public
-    {
-      name: "og:image",
-      content: `${CANONICAL_URL}/logo.png`,
-    },
-    { name: "og:url", content: CANONICAL_URL },
-    { name: "og:type", content: "article" },
-    { name: "og:locale", content: "en_US" },
   ],
 }
